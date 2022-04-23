50ba18b2cd730894dc9c93ffb42ee3a4e5690c39
Hakan Kaya <hakaya@outlook.com>
2022-04-23 18:27:39 +0300

    Initial Commit

---
+++ SuperKahraman/SuperKahramanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperKahramanApp.swift
+//  SuperKahraman
+//
+//  Created by Hakan Kaya on 23.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperKahramanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
